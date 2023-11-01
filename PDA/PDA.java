6451d14bc77121ad0c71b08f356a6dc5436ccf12
Cedar Drake <cedar.drake@mrjaffesclass.com>
2023-11-01 11:45:15 -0700

    Day 2

--- PDA/PDA.java
+++ PDA/PDA.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 /**
  * Permissible Dating Age program
  *
@@ -6,21 +9,45 @@
  */
 public class PDA
 {
-
-    /**
-     * Constructor for objects of class PDA
-     */
+    Scanner scanner = new Scanner(System.in);
+    int age = 0;
+    int LOWER_BOUND = 0;
+    float ageLow = age;
+    float ageHigh = age;
     public PDA()
     {
         // We don't need to do anything in the constructor for
         // our program.
+    }  
+    public void getYoungerAge(int age) {
+        float ageLow = age;
+        ageLow = (age/2) + 7;
+        Math.round(ageLow);
     }
-    
+    public void getOlderAge(int age) {
+        float ageHigh = age;
+        ageHigh = (age + 7) * 2;
+        Math.round(ageHigh);
+    }
     /**
      * This is the main event loop for our PDA program
      */
     public void runEventLoop() {
-        System.out.println("PDA!");        
+        while (true) {
+            System.out.println("How old are you?");
+            try {
+                age = scanner.nextInt();
+                System.out.println(age);
+                if (age < LOWER_BOUND) {
+                    System.out.println(age + " is too young!");
+                } else  {
+                    System.out.println("You're age range is " + ageLow + ageHigh);
+                }
+            } catch (InputMismatchException error) {
+                System.out.println("Please enter an integer");
+                scanner.next();
+            }
+        }
     }
 
     /**
